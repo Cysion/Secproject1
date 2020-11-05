21cc9cefcaf9e5e6f73c7ee66c9d4c3c466b3ecc
Ludwig Wideskär <ludwig.wideskar@gmail.com>
2020-11-05 10:53:09 +0100

    profile edit

--- userprofile/views.py
+++ userprofile/views.py
@@ -21,13 +21,26 @@ def ProfileView(request):
     return render(request, 'userprofile/profile.html', args)
 
 def EditProfileView(request):
+    user1=User.objects.filter(UserId=request.session['UserId'])[0]
+    firstName=user1.getFirstName(request.session['privKey'])
+    lastName=user1.getLastName(request.session['privKey'])
+    dateOfBirth=user1.getDateOfBirth(request.session['privKey'])
+    gender=user1.getGender(request.session['privKey'])
+
+    account = {
+        "firstName":firstName,
+        "lastName":lastName,
+        "dateOfBirth":dateOfBirth,
+        "gender":gender
+    }
+
     profile_lang = get_lang(sections=["userprofile"])
     login_lang = get_lang(sections=["login"])
     args = {
         'menu_titles': UNIVERSAL_LANG["universal"]["titles"],
         'form': login_lang["login"]["form"],
-        'profile': profile_lang["userprofile"]["long_texts"]
+        'profile': profile_lang["userprofile"]["long_texts"],
+        "account":account
     }
 
-    
     return render(request, 'userprofile/edit.html', args)
