26b29b009e3bb68d775139a5788c2ec361030429
cysion <cysion@protonmail.com>
2020-11-09 14:14:06 +0100

    wsgi conf

--- wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ََAPPNAME.settings')
-application = Cling(get_wsgi_application())
