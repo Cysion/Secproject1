30df8a4a4c5ed67b2e6be9f6bbaf770855d51154
cysion <cysion@protonmail.com>
2020-11-10 11:53:35 +0100

    symkey gen and bench

--- tools/compressor.py
+++ tools/compressor.py
@@ -1,16 +1,13 @@
 import gzip
-from logman import get_logger
 
-def compress(path, addtime, logger = get_logger("compressor")):
+def compress(path, addtime):
     """compress the file at path to path.gz, returns path.gz if successful, 0 if failed"""
     try:
-        logger.debug("attempting to compress: " + path)
         with open(path) as compfile:
             with open(path + ".gz", "wb") as compedfile:
                 compedfile.write(gzip.compress(compfile))
 
     except FileNotFoundError:
-        logger.debug("File not found: " + path)
         return 0
 
     else:
