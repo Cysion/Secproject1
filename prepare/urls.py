a10d2a8829f2d5a5167e91b44e122e30b0765e12
Ludwig Wideskär <ludwig.wideskar@gmail.com>
2020-11-19 15:37:34 +0100

    Added the beginning of the prepare app

--- prepare/urls.py
+++ prepare/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = 'prepare'
 urlpatterns = [
     # ex: /polls/
-    #path('', views.IndexView.as_view(), name='index'),
+    path('', views.MenuView, name='Menu')
+    #path('1/', views.HowToView, name='How-To'),
+    #path('2/', views.PracticeBreathingView, name='Practice-Breathing'),
+    #path('3/', views.SupportiveMemoriesView, name='Supportive-Memories'),
+    #path('4/', views.DestructiveMemoriesView, name='Destructive-Memories'),
+    #path('', views.IndexView, name='index'),
 ]
--- prepare/views.py
+++ prepare/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
+
+
+from login.models import User
+#from django.db import transaction
+#from tools.crypto import gen_rsa, secret_scrambler, rsa_encrypt, rsa_decrypt
+#from userprofile.views import checkPassword, changePass
+
+from tools.confman import get_lang
+
+UNIVERSAL_LANG = get_lang(sections=["universal"])
+
+def MenuView(request):
+    prepare_lang = get_lang(sections=["prepare"])
+    args = {
+        'menu_titles': UNIVERSAL_LANG["universal"]["titles"],
+        'back': UNIVERSAL_LANG["universal"]["back"],
+        'prepare': prepare_lang["prepare"]
+    }
+    return render(request, 'prepare/menu.html', args)
