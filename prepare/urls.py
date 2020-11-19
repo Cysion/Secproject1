11d16bc0b28ea107e1430e07013186e15de72396
Kevin <kevin.engstrom@hotmail.se>
2020-11-19 16:05:15 +0100

    Merge fix

--- prepare/urls.py
+++ prepare/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'prepare'
 urlpatterns = [
     # ex: /polls/
-    path('memory/add/', views.addMemoryView, name='add-memory')
+    path('memory/add/', views.addMemoryView, name='add-memory'),
+    path('memory/', views.MemoryView, name='memory')
 ]
