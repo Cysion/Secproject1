d5d2ed7cb767970604e6fcdf7a53091ba091d23b
Robin Lenz <role18@student.bth.se>
2020-11-23 16:41:09 +0100

    Client profile view

--- professionals/views.py
+++ professionals/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from userprofile.views import showAllRelationsFrom, updateRelationTo
-
+from userprofile.views import showAllRelationsFrom, updateRelationTo, sharesDataWith
+from userprofile.models import RelationFrom, RelationTo
+from login.models import User
 # Create your views here.
 
 from tools.confman import get_lang  # Needed for retrieving text from language file
@@ -40,11 +41,46 @@ def ClientsView(request):
 
     return render(request, 'professionals/clients.html', args)
 
+
+
+def profileView(request, UserId):
+    if not 'UserId' in request.session.keys():
+        return HttpResponseRedirect(reverse('login:Login'))
+    
+    userPrivKey = sharesDataWith(UserId, request.session['UserId'], request.session['privKey']).decode("utf-8")
+    if not userPrivKey:
+        return HttpResponseRedirect(reverse('professionals:clients'))
+    user=User.objects.filter(UserId=UserId)[0]
+    
+    account = {}
+    account['firstName']=user.getFirstName(userPrivKey)
+    account['lastName']=user.getLastName(userPrivKey)
+    account['gender']=user.getGender(userPrivKey)
+    account['email'] = user.getEmail()
+
+    profile_lang = get_lang(sections=["userprofile"])
+    login_lang = get_lang(sections=["login"])
+    args = {
+        'menu_titles': UNIVERSAL_LANG["universal"]["titles"],
+        'back': UNIVERSAL_LANG["universal"]["back"],
+        'form': login_lang["login"]["form"],
+        'profile': profile_lang["userprofile"]["long_texts"],
+        'alerts': login_lang['login']['long_texts']['alerts'],
+        "account":account,
+        'template': "base_professionals.html",
+        'profView': True
+    }
+
+    return render(request, 'userprofile/edit.html', args)
+
+
+
 def saveMePlanView(request, UserId):
     if not 'UserId' in request.session.keys():  # This is a check if a user is logged in.
         return HttpResponseRedirect(reverse('login:Login'))
 
-    # YOUR CODE HERE
+    userPrivKey = sharesDataWith(UserId, request.session['UserId'], request.session['privKey']).decode("utf-8")
+    user=User.objects.filter(UserId=UserId)[0]
 
     global_alerts = []  # The variable which is sent to template
     if "global_alerts" in request.session.keys():  # Check if there is global alerts
@@ -62,7 +98,8 @@ def CheckView(request, UserId):
     if not 'UserId' in request.session.keys():  # This is a check if a user is logged in.
         return HttpResponseRedirect(reverse('login:Login'))
 
-    # YOUR CODE HERE
+    userPrivKey = sharesDataWith(UserId, request.session['UserId'], request.session['privKey']).decode("utf-8")
+    user=User.objects.filter(UserId=UserId)[0]
 
     global_alerts = []  # The variable which is sent to template
     if "global_alerts" in request.session.keys():  # Check if there is global alerts
