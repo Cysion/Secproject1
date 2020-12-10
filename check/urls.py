a22a03caacd7b97bd6776752010fbbd8856c2dbc
Kevin <me@kengstrom.se>
2020-12-10 17:55:51 +0100

    Added a default redirect

--- check/urls.py
+++ check/urls.py
@@ -6,6 +6,7 @@ app_name = 'check'
 urlpatterns = [
     # ex: /polls/
     #path('', views.IndexView.as_view(), name='index'),
+    path('', views.GreenCaseView, name="redir-green"),
     path('1/', views.GreenCaseView, name="green-case"),
     path('2/', views.WellFeelingView, name="well-feeling"),
     path('3/', views.SaveMePlanView, name="save-me-plan"),
