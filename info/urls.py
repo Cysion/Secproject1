ec92658896f51e914992be7a601e3f30885e84a1
Ludwig Wideskär <ludwig.wideskar@gmail.com>
2020-12-16 14:14:53 +0100

    Info page done

--- info/urls.py
+++ info/urls.py
@@ -6,4 +6,10 @@ app_name = 'info'
 urlpatterns = [
     # ex: /polls/
     #path('', views.IndexView.as_view(), name='index'),
+    path('', views.MenuView, name="menu"),
+    path('about/', views.AboutView, name="about"),
+    path('howto/', views.HowToView, name="how-to"),
+    path('privacy_gdpr/', views.PrivacyGDPRView, name="privacy-gdpr"),
+    path('volunteering_disclaimer/', views.VolunteeringDisclaimerView, name="volunteering-disclaimer"),
+    path('tos/', views.ToSView, name="terms-of-service")
 ]
